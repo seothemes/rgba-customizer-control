e88b27d359e73592d5c70cf633f2dee3266742eb
seothemes <seothemeswp@gmail.com>
2018-07-09 00:56:04 +0800

    Add composer.json and rename files

--- rgba-customizer-control.php
+++ rgba-customizer-control.php
@@ -19,75 +19,116 @@
  * You should have received a copy of the GNU General Public License
  * along with this Alpha Color Picker. If not, see <http://www.gnu.org/licenses/>.
  */
-class Customize_Alpha_Color_Control extends WP_Customize_Control {
+
+/**
+ * RGBA Color Picker Customizer Control
+ *
+ * This control adds a second slider for opacity to the stock
+ * WordPress color picker, and it includes logic to seamlessly
+ * convert between RGBa and Hex color values as opacity is
+ * added to or removed from a color.
+ */
+class RGBA_Customizer_Control extends WP_Customize_Control {
 
 	/**
 	 * Official control name.
+	 *
+	 * @var string $type Control name.
 	 */
 	public $type = 'alpha-color';
 
 	/**
 	 * Add support for palettes to be passed in.
 	 *
-	 * Supported palette values are true, false, or an array of RGBa and Hex colors.
+	 * Supported values are true, false, or an array of RGBa and Hex colors.
+	 *
+	 * @var array $palette Color palettes.
 	 */
 	public $palette;
 
 	/**
 	 * Add support for showing the opacity value on the slider handle.
+	 *
+	 * @var bool $show_opacity Show opacity.
 	 */
 	public $show_opacity;
 
 	/**
 	 * Enqueue scripts and styles.
 	 *
-	 * Ideally these would get registered and given proper paths before this control object
-	 * gets initialized, then we could simply enqueue them here, but for completeness as a
-	 * stand alone class we'll register and enqueue them here.
+	 * Ideally these would get registered and given proper paths before this
+	 * object is initialized, then we could simply enqueue them here, but
+	 * for completeness as a stand alone class we'll enqueue them here.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return void
 	 */
 	public function enqueue() {
+
 		wp_enqueue_script(
-			'alpha-color-picker',
-			get_stylesheet_directory_uri() . '/admin/customizer/alpha-color-picker/alpha-color-picker.js',
-			array( 'jquery', 'wp-color-picker' ),
+			'rgba-customizer-control',
+			dirname( __FILE__ ) . '/rgba-customizer-control.js',
+			array( 'jquery', 'wp-customizer-control' ),
 			'1.0.0',
 			true
 		);
+
 		wp_enqueue_style(
-			'alpha-color-picker',
-			get_stylesheet_directory_uri() . '/admin/customizer/alpha-color-picker/alpha-color-picker.css',
-			array( 'wp-color-picker' ),
+			'rgba-customizer-control',
+			dirname( __FILE__ ) . '/rgba-customizer-control.css',
+			array( 'wp-customizer-control' ),
 			'1.0.0'
 		);
+
 	}
 
 	/**
 	 * Render the control.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return void
 	 */
 	public function render_content() {
 
-		// Process the palette
+		// Process the palette.
 		if ( is_array( $this->palette ) ) {
+
 			$palette = implode( '|', $this->palette );
+
 		} else {
+
 			// Default to true.
 			$palette = ( false === $this->palette || 'false' === $this->palette ) ? 'false' : 'true';
+
 		}
 
 		// Support passing show_opacity as string or boolean. Default to true.
 		$show_opacity = ( false === $this->show_opacity || 'false' === $this->show_opacity ) ? 'false' : 'true';
 
-		// Begin the output. ?>
+		// Begin the output.
+		if ( isset( $this->label ) && '' !== $this->label ) {
+
+			echo '<span class="customize-control-title">' . esc_html( $this->label ) . '</span>';
+
+		}
+
+		?>
 		<label>
-			<?php // Output the label and description if they were passed in.
-			if ( isset( $this->label ) && '' !== $this->label ) {
-				echo '<span class="customize-control-title">' . sanitize_text_field( $this->label ) . '</span>';
-			}
+			<?php
 			if ( isset( $this->description ) && '' !== $this->description ) {
-				echo '<span class="description customize-control-description">' . sanitize_text_field( $this->description ) . '</span>';
-			} ?>
-			<input class="alpha-color-control" type="text" data-show-opacity="<?php echo $show_opacity; ?>" data-palette="<?php echo esc_attr( $palette ); ?>" data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?>  />
+
+				echo '<span class="description customize-control-description">' . esc_html( $this->description ) . '</span>';
+
+			}
+			?>
+			<input class="alpha-color-control" type="text" data-show-opacity="<?php echo esc_html( $show_opacity ); ?>"
+			       data-palette="<?php echo esc_attr( $palette ); ?>"
+			       data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?> />
 		</label>
 		<?php
+
 	}
+
 }
